2dd3e088c2cd32846e764b7c700bf05203e4a16b
Stephane80 <stephane.dailly@gmail.com>
2018-12-16 19:12:16 +0100

    #define S_CURVE_ACCELERATION
    #define ADAPTIVE_STEP_SMOOTHING
    #define DIGIPOT_I2C_MOTOR_CURRENTS { 0.9, 0.9, 0.9, 0.9, 0.9 }

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -529,7 +529,7 @@
  * vibration and surface artifacts. The algorithm adapts to provide the best possible step smoothing at the
  * lowest stepping frequencies.
  */
-//#define ADAPTIVE_STEP_SMOOTHING
+#define ADAPTIVE_STEP_SMOOTHING
 
 /**
  * Custom Microstepping
@@ -589,7 +589,7 @@
 #define DIGIPOT_I2C_NUM_CHANNELS 5 // 5DPRINT: 4     AZTEEG_X3_PRO: 8     MKS SBASE: 5
 // Actual motor currents in Amps. The number of entries must match DIGIPOT_I2C_NUM_CHANNELS.
 // These correspond to the physical drivers, so be mindful if the order is changed.
-#define DIGIPOT_I2C_MOTOR_CURRENTS { 0.8, 0.8, 0.8, 0.8, 0.8 }  //  MKS SBASE: 5
+#define DIGIPOT_I2C_MOTOR_CURRENTS { 0.9, 0.9, 0.9, 0.9, 0.9 }  //  MKS SBASE: 5
 
 //===========================================================================
 //=============================Additional Features===========================
